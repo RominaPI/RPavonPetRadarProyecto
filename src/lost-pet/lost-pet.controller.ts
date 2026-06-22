ebaa9d028f654adf96fe84e4d289b605fe578f2f
RominaPI <rominapavonibarra@gmail.com>
2026-06-21 23:04:18 -0600

    get lost pets fixed

--- src/lost-pet/lost-pet.controller.ts
+++ src/lost-pet/lost-pet.controller.ts
@@ -10,7 +10,7 @@ export class LostPetController {
 
     @Get()
     async getLostPets() {
-        const res = await this.losPetService.getLostPets;
+        const res = await this.losPetService.getLostPets();
         return res;
     }
     
